15d9a86b86430cebeaf8a74a2d0e31c73f894dde
imp <imp.sergei@gmail.com>
2021-07-21 00:11:07 +0300

    Initial commit

---
+++ src/main/java/com/tregulov/spring/security/configuration/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.tregulov.spring.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
